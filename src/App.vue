3098071d3602420f5fd442ac7500deffac999d74
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-17 22:51:20 +0200

    211 POSTing Data to a Server Sending a POST Request

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Http</h1>
+        <div class="form-group">
+          <label>Username</label>
+          <input type="text" class="form-control" v-model="user.username">
+        </div>
+        <div class="form-group">
+          <label>Mail</label>
+          <input type="text" class="form-control" v-model="user.email">
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
       </div>
     </div>
   </div>
@@ -10,6 +19,25 @@
 
 <script>
   export default {
+    data() {
+      return {
+        user: {
+          username: '',
+          email: ''
+        }
+      };
+    },
+    methods: {
+      submit() {
+        this.$http.post('https://vuejs-http-23956.firebaseio.com/data.json', this.user)
+          .then(response => {
+              console.log(response);
+            }, error => {
+              console.log(error);
+            });
+
+      }
+    }
   }
 </script>
 
