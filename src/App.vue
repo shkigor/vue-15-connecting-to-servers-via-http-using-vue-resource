0ece4855a606d37babf887ca1d6db25966383b6b
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-18 14:35:39 +0200

    219 Understanding Template URLs
    https://github.com/pagekit/vue-resource
    URI Templates
    https://medialize.github.io/URI.js/uri-template.html

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
         </div>
         <button class="btn btn-primary" @click="submit">Submit</button>
         <hr>
+        <input type="text" class="form-control" v-model="node">
+        <br><br>
         <button class="btn btn-primary" @click="fetchData">Get Data</button>
         <ul class="list-group">
           <li class="list-group-item" v-for="u in users">{{ u.username }} - {{ u.email }}</li>
@@ -31,7 +33,8 @@
           email: ''
         },
         users: [],
-        resource: {}
+        resource: {},
+        node: 'data'
       };
     },
     methods: {
@@ -47,24 +50,36 @@
           this.resource.saveAlt(this.user);
       },
       fetchData() {
-        this.$http.get('data.json')
-          .then(response => {
-              return response.json();
-          })
-          .then(data => {
-              const resultArray = [];
-              for (let key in data) {
-                  resultArray.push(data[key]);
-              }
-              this.users = resultArray;
-          });
+        // this.$http.get('data.json')
+        //   .then(response => {
+        //       return response.json();
+        //   })
+        //   .then(data => {
+        //       const resultArray = [];
+        //       for (let key in data) {
+        //           resultArray.push(data[key]);
+        //       }
+        //       this.users = resultArray;
+        //   });
+        this.resource.getData({node: this.node})
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                const resultArray = [];
+                for (let key in data) {
+                    resultArray.push(data[key]);
+                }
+                this.users = resultArray;
+            });
       }
     },
     created() {
         const customActions = {
-            saveAlt: {method: 'POST', url: 'alternative.json'}
+            saveAlt: {method: 'POST', url: 'alternative.json'},
+            getData: {method: 'GET'}
         };
-        this.resource = this.$resource('data.json', {}, customActions);
+        this.resource = this.$resource('{node}.json', {}, customActions);
     }
   }
 </script>
